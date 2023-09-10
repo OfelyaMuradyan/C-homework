0a0f52974d53a75618d3cc6a11af9efd3edae650
OfelyaMuradyan <138681012+OfelyaMuradyan@users.noreply.github.com>
2023-09-10 17:14:57 +0400

    Add files via upload

---
+++ g19.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr1[] = {1,2,3,4,5,6};
+	size_t k = sizeof(arr1) / sizeof(arr1[0]);
+	int arr2[k];
+	int i = 0;
+
+	for(i = 0;i < k; i++)
+	{
+		arr2[i] = arr1[i];
+	}
+
+	for(i = 0; i < k; i++)
+	{
+		if(arr1[i] == arr2[i])
+		{
+			printf("%d\n", arr2[i]);
+		}
+		else
+		{
+			printf("It isn't valid.");
+			return 0;
+		}
+	}
+
+}
